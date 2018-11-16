1a4eed0595a589b720846a227978e0426a55cd27
Scott Bergler <commandinghands@gmail.com>
2018-11-16 08:35:39 -0800

    add initial project directories, files, and basic README.

---
+++ ModelTests/RepeatCounterTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class RepeatCounterTest
+  {
+    // Test methods go here
+  }
+}
