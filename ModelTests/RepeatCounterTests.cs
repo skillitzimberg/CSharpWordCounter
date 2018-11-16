fab4a29d41b315a857556391a98530b75bef7408
Scott Bergler <commandinghands@gmail.com>
2018-11-16 09:52:34 -0800

    add first spec test-passes; add second test-not tested.

--- ModelTests/RepeatCounterTests.cs
+++ ModelTests/RepeatCounterTests.cs
@@ -1,5 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using WordCounter;
+using WordCounter.Models;
 
 namespace WordCounter.Tests
 {
@@ -7,5 +7,21 @@ namespace WordCounter.Tests
   public class RepeatCounterTest
   {
     [TestMethod]
+    public void RepeatCounterConstructor_CreatesInstanceOfReapeatCounter_RepeatCounter()
+    {
+      RepeatCounter newRepeatCounter = new RepeatCounter();
+      Assert.AreEqual(typeof(RepeatCounter), newRepeatCounter.GetType());
+    }
+
+    [TestMethod]
+    public void GetWordToFind_ReturnsWordToFind_WordToFind()
+    {
+      string expectedString = "hello";
+      RepeatCounter newRepeatCounter = new RepeatCounter(expectedString);
+
+      string actualString = newRepeatCounter.GetWordToFind();
+
+      Assert.AreEqual(expectedString, actualString)
+    }
   }
 }
