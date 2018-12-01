fe4ff0fb18d723ac2bbaf3cd819ec4289e3f4886
Scott Bergler <commandinghands@gmail.com>
2018-12-01 11:08:51 -0800

    edit specifications for clarity and practicality.

--- README.md
+++ README.md
@@ -9,58 +9,59 @@ The goal of this project is to write a program that gathers both a word and sent
 
 Each spec should include a specific input and output, and a description sentence explaining why you selected that input value as the simplest way to test the behavior your spec covers.
 
-### MoSpecifications:
-##### Spec 1: Counter can hold a word to search for in later specs.
-**Example:** The original input was a single letter 'a'. The output was the same letter 'a'.
-
-This is a property that is passed in each time the program is run and I wanted to make sure it was accessible. I chose a letter because I knew that I was going to be looking for letters in later specs.
-
-The input for StringToSearch was added later as the class/constructor now had two properties and passing in a single property caused compilation errors.
-
-##### Spec 2: Counter can hold a string to be searched in later specs.
-**Example:** The input is the word 'searched' and the phrase 'This string is to be searched for WordToFind' The output is the phrase 'This string is to be searched for WordToFind'.
-
-I chose these inputs because I was trying to be semantic. This spec accomplishes a similar goal as the previous spec but incrementally adds to the program's capabilities.
-
-##### Spec 3: Counter can compare its strings when they are both letters.
-**Example:** The inputs for this spec are the single letters 'w'. The output is the boolean true.
-
-I chose single letters (which single letter didn't seem important) as they are the simplest relevant things to compare. I wanted to be sure that Counter could 'see' and report the value of its properties.
-
-##### Spec 4: Counter can find the first string as a letter in the second string when it is not part of a word.
-**Example:** The first string is the letter 'a'; the second string is 'a cat' - the program will find the word 'a', but ignore the word 'cat'. The output is boolean true.
-
-I chose these inputs because they were the smallest step up from comparing two letters and presented a potential stumbling block if the 'a' in 'cat' was found.
-
-I am aware I'm not using the this nor the three methods above for the functionality of the program, but I thought they both handled the spec and might be useful in next week's work.
-
-Also, as noted below, I messed up this spec by trying to do too much and things got confusing around this point and work started to straddle the fifth and sixth specs.
-
-##### Spec 5: Counter can find the first string as a word in the second string when it is not part of a word.
-**Example:** The first string is the word 'dog'; the second string is 'the dog dodged doggedly' - the program will find the word 'dog', but ignore the word 'doggedly'.
-
-As for the previous spec, these inputs seemed the smallest relevant step to take and also double checked that the 'dog' in 'doggedly' would be ignored. I later changed the input to 'an' and output to 'a cat that saw a rat and a dog had an anuerysm.' because I wanted to make sure that the program was finding the word when it wasn't part of a word and was looking at every word in the string, not stopping part way through. This was in part due to some issues I was troubleshooting due to my making the fifth spec too broad.
-
-##### Spec 6: Counter can find every instance of the first string as a word in the second string when it is not part of a word.
-**Example:** The first string is the word 'happy'; the second string is 'I felt happy because I saw the others were happy and because I knew I should feel happy, but I wasn’t really happy.' - the program will find each instance of the word 'happy'.
-
-Again, this is the smallest step forward from the previous spec, increasing the complexity very subtly.
-
-It also ended up providing a good case to ignore punctuation.
-
-##### Spec 7: Counter can find every instance of the first string as a word in the second string when it is not part of a word and count how many instances were found.
-**Example:** The first string is the word 'happy'; the second string is 'I felt happy because I saw the others were happy and because I knew I should feel happy, but I wasn’t really happy.' - the program will find each instance of the word 'happy' and return the count 4.
-
-This is also the next smallest logical step from the previous spec.
-
-##### Spec 8: Counter can find instances of the first string as a word in the second string when it is capitalized.
-**Example:** The first string is the word 'windy'; the second string is 'Is the Windy City always windy?' - the program will find each instance of the word 'windy' and return the count 2.
-
-##### Spec 9: Counter can retrieve its \_matchCount Property.
+### Model Specifications:
+##### Spec 1: Counter returns a string input.
 **Example:**
-Input to find: "c";
-Input to search: "c c c c"
-Output: 4.
+Input: 'a'.
+Output: 'a'
+
+A single letter is the simplest input that is a word.
+
+##### Spec 2: Counter returns two string inputs.
+**Example:**
+InputOne: 'a'
+InputTwo: 'b'
+Output: 'a', 'b'
+
+Checks to make sure both inputs are returned and cannot be confused with each other.
+
+##### Spec 3: Counter compares both inputs and returns boolean: true if they match, boolean: false if they don't.
+**Example:**
+InputOne: 'a'
+InputTwo: 'banana'
+Output: 'false'
+
+Confirms that inputs are being compared to each other rather than having InputOne found in InputTwo when InputTwo is a multi-letter word.
+
+##### Spec 4: Counter can verify that InputOne appears in InputTwo when InputTwo contains two or more words and returns boolean: true if it does, boolean: false if it doesn't.
+**Example:**
+InputOne: 'banana'
+InputTwo: 'one banana'
+Output: 'true'
+
+Confirms that Counter doesn't stop comparing after the first word.
+
+##### Spec 5: Counter counts every instance of InputOne in InputTwo and returns that number.
+**Example:**
+InputOne: 'banana'
+InputTwo: 'banana cabana bananas banana annabanana another banana'
+Output: '3'
+
+Confirms that Counter can add instances found and return that number.
+
+##### Spec 6: Counter ignores capitalization and returns correct number of matches.
+**Example:**
+InputOne: 'bAnana'
+InputTwo: 'Banana cabana bananas baNana annabanana another bananA'
+Output: '3'
+
+Confirms that Counter can return the correct count regardless of capitalization.
+
+##### Spec 7: Counter ignores punctuation and returns correct number of matches.
+**Example:**
+InputOne: 'happy'
+InputTwo: 'I felt happy because I saw the others were .happy and because I knew I should feel happy,. but I wasn’t really happy.'
+Output: '4'
 
 
 ## Setup/Installation Requirements
--- WordCounter.Tests/ModelTests/CounterTests.cs
+++ WordCounter.Tests/ModelTests/CounterTests.cs
@@ -11,131 +11,131 @@ namespace WordCounter.Tests
     [TestMethod]
     public void Get_wordToFind_ReturnsWordToFind_WordToFind()
     {
-      string wordToFind = "hello";
-      string stringToSearch = "hello, world";
+      string wordToFind = "a";
+      string stringToSearch = "b";
       Counter newCounter = new Counter(wordToFind, stringToSearch);
 
       string actualString = newCounter.Get_wordToFind();
 
       Assert.AreEqual(wordToFind, actualString);
     }
-
-    [TestMethod]
-    public void Get_stringToSearch_ReturnsTheStringToSearch_StringToSearch()
-    {
-      string wordToFind = "searched";
-      string expectedStringToSearch = "This string is to be searched for WordToFind";
-
-      Counter newCounter = new Counter(wordToFind, expectedStringToSearch);
-
-      string actualStringToSearch = newCounter.Get_stringToSearch();
-
-      Assert.AreEqual(expectedStringToSearch, actualStringToSearch);
-    }
-
-    [TestMethod]
-    public void Set_matchCount_SetsNumberOfWordInstances_Int()
-    {
-      //Arrange
-      string wordToFind = "happy";
-      string expectedStringToSearch = "I felt happy because I saw the others were happy and because I knew I should feel happy, but I wasn’t really happy.";
-      int expectedCount = 4;
-
-      Counter newCounter = new Counter(wordToFind, expectedStringToSearch);
-
-      //Act
-      int count = newCounter.CountHowManyTimesTheWordWasFound();
-      newCounter.Set_matchCount(count);
-      int actualCount = newCounter.Get_matchCount();
-
-      //Assert
-      Assert.AreEqual(expectedCount, actualCount);
-    }
-
-    [TestMethod]
-    public void Get_matchCount_ReturnsNumberOfWordInstances_Int()
-    {
-      string wordToFind = "happy";
-      string expectedStringToSearch = "I felt happy because I saw the others were happy and because I knew I should feel happy, but I wasn’t really happy.";
-      int expectedCount = 4;
-
-      Counter newCounter = new Counter(wordToFind, expectedStringToSearch);
-
-      int count = newCounter.CountHowManyTimesTheWordWasFound();
-      newCounter.Set_matchCount(count);
-      int actualCount = newCounter.Get_matchCount();
-
-      // Console.WriteLine("CounterTests: " + actualCount);
-
-      Assert.AreEqual(expectedCount, actualCount);
-    }
-
-    [TestMethod]
-    public void _wordToFindMatches_ConfirmWhetherStringsMatchOrNot_True()
-    {
-      string wordToFind = "w";
-      string stringToSearch = "w";
-
-      Counter newCounter = new Counter(wordToFind, stringToSearch);
-
-      bool doStringsMatch = newCounter._wordToFindMatches(stringToSearch);
-
-      Assert.AreEqual(true, doStringsMatch);
-    }
-
-    [TestMethod]
-    public void FindMatchingWords_ReturnAListOfMatchingWordsFound_ListOfFoundMatches()
-    {
-      string wordToFind = "an";
-      string stringToSearch = "a cat that saw a rat and a dog had an anuerysm.";
-      List<string> expectedList = new List<string> {"an"};
-
-      Counter newCounter = new Counter(wordToFind, stringToSearch);
-
-      List<string> actualList = newCounter.FindMatchingWords();
-
-      CollectionAssert.AreEqual(expectedList, actualList);
-    }
-
-    [TestMethod]
-    public void FindMatchingWords_ReturnAListOfMultipleMatchingWordsFound_ListOfFoundMatches()
-    {
-      string wordToFind = "happy";
-      string stringToSearch = "I felt happy because I saw the others were happy and because I knew I should feel happy, but I wasn’t really happy.";
-      List<string> expectedList = new List<string> {"happy", "happy", "happy", "happy"};
-
-      Counter newCounter = new Counter(wordToFind, stringToSearch);
-
-      List<string> actualList = newCounter.FindMatchingWords();
-
-      CollectionAssert.AreEqual(expectedList, actualList);
-    }
-
-    [TestMethod]
-    public void CountHowManyTimesTheWordWasFound_ReturnTheNumberOfTimesTheWordWasFound_Integer()
-    {
-      string wordToFind = "happy";
-      string stringToSearch = "I felt happy because I saw the others were happy and because I knew I should feel happy, but I wasn’t really happy.";
-      int expectedCount = 4;
-
-      Counter newCounter = new Counter(wordToFind, stringToSearch);
-
-      int actualCount = newCounter.CountHowManyTimesTheWordWasFound();
-
-      Assert.AreEqual(expectedCount, actualCount);
-    }
-
-    [TestMethod]
-    public void _wordToFindMatches_MatchesWordsCaseInsensitive_True()
-    {
-      string wordToFind = "w";
-      string stringToSearch = "W";
-
-      Counter newCounter = new Counter(wordToFind, stringToSearch);
-
-      bool doStringsMatch = newCounter._wordToFindMatches(stringToSearch);
-
-      Assert.AreEqual(true, doStringsMatch);
-    }
+    //
+    // [TestMethod]
+    // public void Get_stringToSearch_ReturnsTheStringToSearch_StringToSearch()
+    // {
+    //   string wordToFind = "searched";
+    //   string expectedStringToSearch = "This string is to be searched for WordToFind";
+    //
+    //   Counter newCounter = new Counter(wordToFind, expectedStringToSearch);
+    //
+    //   string actualStringToSearch = newCounter.Get_stringToSearch();
+    //
+    //   Assert.AreEqual(expectedStringToSearch, actualStringToSearch);
+    // }
+    //
+    // [TestMethod]
+    // public void Set_matchCount_SetsNumberOfWordInstances_Int()
+    // {
+    //   //Arrange
+    //   string wordToFind = "happy";
+    //   string expectedStringToSearch = "I felt happy because I saw the others were happy and because I knew I should feel happy, but I wasn’t really happy.";
+    //   int expectedCount = 4;
+    //
+    //   Counter newCounter = new Counter(wordToFind, expectedStringToSearch);
+    //
+    //   //Act
+    //   int count = newCounter.CountHowManyTimesTheWordWasFound();
+    //   newCounter.Set_matchCount(count);
+    //   int actualCount = newCounter.Get_matchCount();
+    //
+    //   //Assert
+    //   Assert.AreEqual(expectedCount, actualCount);
+    // }
+    //
+    // [TestMethod]
+    // public void Get_matchCount_ReturnsNumberOfWordInstances_Int()
+    // {
+    //   string wordToFind = "happy";
+    //   string expectedStringToSearch = "I felt happy because I saw the others were happy and because I knew I should feel happy, but I wasn’t really happy.";
+    //   int expectedCount = 4;
+    //
+    //   Counter newCounter = new Counter(wordToFind, expectedStringToSearch);
+    //
+    //   int count = newCounter.CountHowManyTimesTheWordWasFound();
+    //   newCounter.Set_matchCount(count);
+    //   int actualCount = newCounter.Get_matchCount();
+    //
+    //   // Console.WriteLine("CounterTests: " + actualCount);
+    //
+    //   Assert.AreEqual(expectedCount, actualCount);
+    // }
+    //
+    // [TestMethod]
+    // public void _wordToFindMatches_ConfirmWhetherStringsMatchOrNot_True()
+    // {
+    //   string wordToFind = "w";
+    //   string stringToSearch = "w";
+    //
+    //   Counter newCounter = new Counter(wordToFind, stringToSearch);
+    //
+    //   bool doStringsMatch = newCounter._wordToFindMatches(stringToSearch);
+    //
+    //   Assert.AreEqual(true, doStringsMatch);
+    // }
+    //
+    // [TestMethod]
+    // public void FindMatchingWords_ReturnAListOfMatchingWordsFound_ListOfFoundMatches()
+    // {
+    //   string wordToFind = "an";
+    //   string stringToSearch = "a cat that saw a rat and a dog had an anuerysm.";
+    //   List<string> expectedList = new List<string> {"an"};
+    //
+    //   Counter newCounter = new Counter(wordToFind, stringToSearch);
+    //
+    //   List<string> actualList = newCounter.FindMatchingWords();
+    //
+    //   CollectionAssert.AreEqual(expectedList, actualList);
+    // }
+    //
+    // [TestMethod]
+    // public void FindMatchingWords_ReturnAListOfMultipleMatchingWordsFound_ListOfFoundMatches()
+    // {
+    //   string wordToFind = "happy";
+    //   string stringToSearch = "I felt happy because I saw the others were happy and because I knew I should feel happy, but I wasn’t really happy.";
+    //   List<string> expectedList = new List<string> {"happy", "happy", "happy", "happy"};
+    //
+    //   Counter newCounter = new Counter(wordToFind, stringToSearch);
+    //
+    //   List<string> actualList = newCounter.FindMatchingWords();
+    //
+    //   CollectionAssert.AreEqual(expectedList, actualList);
+    // }
+    //
+    // [TestMethod]
+    // public void CountHowManyTimesTheWordWasFound_ReturnTheNumberOfTimesTheWordWasFound_Integer()
+    // {
+    //   string wordToFind = "happy";
+    //   string stringToSearch = "I felt happy because I saw the others were happy and because I knew I should feel happy, but I wasn’t really happy.";
+    //   int expectedCount = 4;
+    //
+    //   Counter newCounter = new Counter(wordToFind, stringToSearch);
+    //
+    //   int actualCount = newCounter.CountHowManyTimesTheWordWasFound();
+    //
+    //   Assert.AreEqual(expectedCount, actualCount);
+    // }
+    //
+    // [TestMethod]
+    // public void _wordToFindMatches_MatchesWordsCaseInsensitive_True()
+    // {
+    //   string wordToFind = "w";
+    //   string stringToSearch = "W";
+    //
+    //   Counter newCounter = new Counter(wordToFind, stringToSearch);
+    //
+    //   bool doStringsMatch = newCounter._wordToFindMatches(stringToSearch);
+    //
+    //   Assert.AreEqual(true, doStringsMatch);
+    // }
   }
 }
